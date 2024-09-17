5d01345e2ae9aea9bb59d9814e60b3e1bb521cfc
khaik105maks <m.buchok@khai.edu>
2024-09-17 23:42:17 +0300

    chore(github): test rebuild

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express();
-app.get('/', (_, res) => res.send('Hello World!'))
+app.get('/', (_, res) => res.send('!!!Hello World!!!!!'))
 app.listen(3000, () => console.log('Server ready'))
