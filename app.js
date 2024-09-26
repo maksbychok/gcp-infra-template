fa1eea6a4a885d7b5fd09212d34348a309ee92be
khaik105maks <m.buchok@khai.edu>
2024-09-27 00:33:52 +0300

    chore(ci|cd): add secret manager

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express();
-app.get('/', (_, res) => res.send('Hello World'))
+app.get('/', (_, res) => res.send(`Hello World ${process.env.MY_SECRET_1}`))
 app.listen(3000, () => console.log('Server ready'))
